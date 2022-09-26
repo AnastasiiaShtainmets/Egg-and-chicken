7d686440defff252f64c320b4ef20fc360572b5b
AnastasiiaShtainmets <nastya589sht@gmail.com>
2022-09-26 14:57:42 +0300

    Egg and chicken

---
+++ src/test/Main.java
@@ -0,0 +1,45 @@
+package test;
+
+public class Main {
+    public static void main(String[] args) {
+        AnimalThread egg = (new AnimalThread("Яйцо"));
+        AnimalThread chicken = (new AnimalThread("Курица"));
+        egg.start();
+        chicken.start();
+
+        try {
+            egg.join();
+            if(chicken.isAlive()){
+                System.out.println("Выиграла курица");
+            }else {
+                chicken.join();
+                System.out.println("Выиграло яйцо");
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
+class AnimalThread extends Thread {
+    String Name;
+    final int distance = 6;
+    public AnimalThread(String Name) {
+
+        this.Name = Name;
+    }
+    @Override
+    public void run() {
+        Thread.currentThread().setName(Name);
+        for (int i = 0; i < distance; i++) {
+            try {
+                Thread.sleep(5);
+                System.out.println(getName());
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
